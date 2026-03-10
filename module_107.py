f831b3c491093fe8acaa0de34b63bc387daa762d
Hacker Hirthik <hackeranonymoushirthik@gmail.com>
2026-03-11 04:15:32 +0530

    FEATURE-107 implement feature

---
+++ module_107.py
@@ -0,0 +1,14 @@
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
+
+def calculate_total(price, tax):
+    return price + price * tax
+
+# reviewer suggestion
+
+class User:
+    def __init__(self,name):
+        self.name=name
