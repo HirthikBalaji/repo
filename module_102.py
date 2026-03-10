d7acf980ef5a866184f814b8c441aec7339c6f6b
Hirthik Balaji <hirthikbalaji2006@gmail.com>
2026-03-11 04:15:30 +0530

    FEATURE-102 implement feature

---
+++ module_102.py
@@ -0,0 +1,14 @@
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
+
+# reviewer suggestion
+
+def calculate_total(price, tax):
+    return price + price * tax
