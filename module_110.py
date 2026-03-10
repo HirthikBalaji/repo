495b090046c3cdfdf739f6c73f752b6404bff658
Asus Dev <asusvivobooks15oled2024@gmail.com>
2026-03-11 04:15:34 +0530

    FEATURE-110 implement feature

---
+++ module_110.py
@@ -0,0 +1,15 @@
+
+def calculate_total(price, tax):
+    return price + price * tax
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+def calculate_total(price, tax):
+    return price + price * tax
+
+# reviewer suggestion
+
+def calculate_total(price, tax):
+    return price + price * tax
