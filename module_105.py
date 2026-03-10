8a8b883c9440e418c01ec9d957b63e7e98357ef9
Anandha Priya <anandhappriya@gmail.com>
2026-03-11 04:15:31 +0530

    FEATURE-105 implement feature

---
+++ module_105.py
@@ -0,0 +1,15 @@
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+# reviewer suggestion
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
