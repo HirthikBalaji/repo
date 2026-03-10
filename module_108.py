9321b157b7c4689e8d2149a9ea4c21c84622914f
Hacker Hirthik <hackeranonymoushirthik@gmail.com>
2026-03-11 04:15:33 +0530

    FEATURE-108 implement feature

---
+++ module_108.py
@@ -0,0 +1,17 @@
+
+def calculate_total(price, tax):
+    return price + price * tax
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
+
+# reviewer suggestion
+
+def calculate_total(price, tax):
+    return price + price * tax
