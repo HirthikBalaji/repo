24d42fb8353a9da3d0f5f6cded193ef7b0227004
Anandha Priya <anandhappriya@gmail.com>
2026-03-11 04:15:31 +0530

    FEATURE-104 implement feature

---
+++ module_104.py
@@ -0,0 +1,17 @@
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+def calculate_total(price, tax):
+    return price + price * tax
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
+
+# reviewer suggestion
+
+def calculate_total(price, tax):
+    return price + price * tax
