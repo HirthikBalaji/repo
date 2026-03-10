c20e6f4c7a3746f26f4e1ecdb86d291ef6a6fb55
Asus Dev <asusvivobooks15oled2024@gmail.com>
2026-03-11 04:15:33 +0530

    FEATURE-109 implement feature

---
+++ module_109.py
@@ -0,0 +1,19 @@
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+class User:
+    def __init__(self,name):
+        self.name=name
+
+def login(user, password):
+    if user == "admin" and password == "admin":
+        return True
+    return False
+
+# reviewer suggestion
+
+class User:
+    def __init__(self,name):
+        self.name=name
